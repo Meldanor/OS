fbf34a5dfab2d10df293677ed1c5b0e629b7edfc
Kilian Gaertner <Bhaals@gmx.de>
2013-05-20 01:16:02 +0200

    implement keyboard driver

--- src/device/keyboard.cc
+++ src/device/keyboard.cc
@@ -10,18 +10,42 @@
 #                    INCLUDES                     #
 \* * * * * * * * * * * * * * * * * * * * * * * * */
 #include "device/keyboard.h"
+#include "useful/plugbox.h"
+#include "useful/pic.h"
+#include "useful/kout.h"
+#include "config.h"
 
 /* * * * * * * * * * * * * * * * * * * * * * * * *\
 #                    METHODS                      # 
 \* * * * * * * * * * * * * * * * * * * * * * * * */
-/** \todo \~german implementieren \~english write implementation */
-Keyboard::Keyboard(){
+
+const unsigned char x = 4;
+const unsigned char y = 10;
+const bool isSecondPIC = PIC::keyboard >= 8;
+
+Keyboard::Keyboard() : Keyboard_Controller() , Gate() {
+
 }
 
-/** \todo \~german implementieren \~english write implementation */
-void Keyboard::plugin(){
+void Keyboard::plugin() {
+	plugbox.assign(PIC::keyboard, *this);
 }
 
-/** \todo \~german implementieren \~english write implementation */
-void Keyboard::trigger(){
+void Keyboard::trigger() {
+	Key key = key_hit();
+	// Invalid key
+	if (!key.valid()) {
+		kout << "Invalid key!" << endl;
+		pic.ack(isSecondPIC );
+		return;	
+	}
+	// CTRL + ALT + DEL = Reboot
+	else if (key.ctrl() && key.alt() && key.scancode() == Key::scan::del) {
+		reboot();
+	}
+	// Otherwise print the key in ascii on a specific position
+	else {
+		kout.show(4,10, key.ascii(), DEFAULT_SCREEN_ATTRIB);
+		pic.ack(isSecondPIC );	
+	}
 }
